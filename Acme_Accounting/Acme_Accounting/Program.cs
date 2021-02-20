075f4b9d532e3224bb2de6a4de9acc4e43966f2d
GHam83 <76718252+GHam83@users.noreply.github.com>
2021-02-20 15:29:44 -0800

    Push Acme
    Push Acme per instructions

---
+++ Acme_Accounting/Acme_Accounting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems!\n We're Accounting on you!");
+        Console.ReadLine();
+    }
+}
