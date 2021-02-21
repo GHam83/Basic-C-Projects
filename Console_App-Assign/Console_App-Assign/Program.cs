1624d6c3e8f794c91df067a1f8e446cb04ddbbd5
GHam83 <76718252+GHam83@users.noreply.github.com>
2021-02-20 17:34:22 -0800

    Push Console App
    Push per pg 158 instructions

---
+++ Console_App-Assign/Console_App-Assign/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Please enter a number: ");
+        long number1 = Convert.ToInt32(Console.ReadLine());
+        long addNum = number1 * 50;
+        Console.WriteLine("Your number times 50 equals " + addNum);
+        Console.ReadLine();
+
+        Console.WriteLine("Please enter a number: ");
+        long number2 = Convert.ToInt32(Console.ReadLine());
+        long addNum2 = number2 + 25;
+        Console.WriteLine("Your number plus 25 is " + addNum2);
+        Console.ReadLine();
+
+        Console.WriteLine("Please enter a number: ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+        double addNum3 = number3 / 12.5;
+        Console.WriteLine("Your number divided by 12.5 equals " + addNum3);
+        Console.ReadLine();
+
+        Console.WriteLine("Please enter a number: ");
+        int number4 = Convert.ToInt32(Console.ReadLine());
+        bool greater = number4 > 50;
+        Console.WriteLine(greater);
+        Console.ReadLine();
+
+        Console.WriteLine("Please enter a number: ");
+        int number5 = Convert.ToInt32(Console.ReadLine());
+        int remainder = number5 % 7;
+        Console.WriteLine("The modulus of your number is " + remainder);
+        Console.ReadLine();
+
+        }
+    }
