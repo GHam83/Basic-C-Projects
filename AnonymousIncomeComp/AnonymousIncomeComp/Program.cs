618c5444d326c18adff34e6bac17250418561427
GHam83 <76718252+GHam83@users.noreply.github.com>
2021-02-21 14:52:12 -0800

    Push Annual Wage Comp
    push per pg 159

---
+++ AnonymousIncomeComp/AnonymousIncomeComp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Anonymous Income Comparison");
+        Console.ReadLine();
+
+        Console.WriteLine("Person 1");
+        Console.ReadLine();
+
+        Console.WriteLine("Hourly Rate?");
+        float wage1 = Convert.ToInt32(Console.ReadLine());
+        Console.ReadLine();
+
+        Console.WriteLine("Hours worked per week?");
+        float hoursPerweek = Convert.ToInt32(Console.ReadLine());
+        Console.ReadLine();
+
+        Console.WriteLine("Person 2");
+        Console.ReadLine();
+
+        Console.WriteLine("Hourly Rate?");
+        float wage2 = Convert.ToInt32(Console.ReadLine());
+        Console.ReadLine();
+
+        Console.WriteLine("Hours worked per week?");
+        float hoursPerweek2 = Convert.ToInt32(Console.ReadLine());
+        Console.ReadLine();
+
+        Console.WriteLine("Annual Salary of Person 1: ");
+        double weeklyWage = wage1 * hoursPerweek;
+        double annualSalary = weeklyWage * 52;
+        Console.WriteLine(annualSalary);
+        Console.ReadLine();
+
+        Console.WriteLine("Annual Salary of Person 2: ");
+        double weeklyWage2 = wage2 * hoursPerweek2;
+        double annualSalary2 = weeklyWage2 * 52;
+        Console.WriteLine(annualSalary2);
+        Console.ReadLine();
+
+        Console.WriteLine("Does Person 1 make more money than Person 2?");
+        Console.ReadLine();
+        bool moreThan = annualSalary > annualSalary2;
+        Console.WriteLine(moreThan);
+        Console.ReadLine();
+
+
+        }
+    }
