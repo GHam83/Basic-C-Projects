81b77fe2bfd8da915d3287227f19ca39d279a95e
GHam83 <76718252+GHam83@users.noreply.github.com>
2021-02-21 16:11:49 -0800

    Push Insurance
    push per pg168

---
+++ InsuranceApproval/InsuranceApproval/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Insurance Approval Form");
+        Console.ReadLine();
+
+        Console.WriteLine("What is your age?");
+        int Age = Convert.ToInt32(Console.ReadLine());
+        bool AgeOkay = Age >= 25;
+        
+       
+
+        Console.WriteLine("Have you ever had a DUI? true or false");
+        string dui = Console.ReadLine();
+        bool duiOk = dui == "false";
+        
+        
+
+        Console.WriteLine("How many speeding tickets do you have?");
+        int tickets = Convert.ToInt32(Console.ReadLine());
+        bool ticketsOk = tickets <= 3;
+        
+        
+
+        Console.WriteLine("Processing...");
+        bool Approval = AgeOkay && duiOk && ticketsOk;
+        Console.WriteLine("Approved?\n " + Approval);
+        Console.ReadLine();
+        }
+    }
