6ad87be526ba0989bc931b8524db1423d9c69d70
GHam83 <76718252+GHam83@users.noreply.github.com>
2021-02-25 17:51:22 -0800

    String Manipulation
    Push per pg193

---
+++ String_Manipulation/String_Manipulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        string first = "hello world! ";
+        string second = "How are you ";
+        string third = "today. It is rainy.";
+        string upper = first.ToUpper();
+
+        StringBuilder sb = new StringBuilder("Greg Hamill");
+        sb.Append("\n is learning how to use String builder");
+        sb.Append("\n in C#. He is enjoying it very");
+        sb.Append("\nmuch. \"So much fun\", he said when asked earlier.");
+
+
+
+        Console.WriteLine(upper + second + third);
+        Console.WriteLine(sb);
+        Console.ReadLine();
+        }
+    }
+
