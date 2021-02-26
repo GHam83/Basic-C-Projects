d6c6dc9d4a6cba931b6f883f03e0a90086093397
GHam83 <76718252+GHam83@users.noreply.github.com>
2021-02-25 17:11:23 -0800

    Push Boolean While
    Push per pg 190

---
+++ boolean_while/boolean_while/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Program
+    {
+    static void Main()
+    {
+        Console.WriteLine("Let's count to ten!");
+        int i = 0;
+        while (i < 11)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+
+        Console.ReadLine();
+        Console.WriteLine("Let's do it again with a different method!");
+        int n = 0;
+        do
+        {
+            Console.WriteLine(n);
+            n++;
+        }
+        while (n < 11);
+        Console.ReadLine();
+    }
+    }
+
+
